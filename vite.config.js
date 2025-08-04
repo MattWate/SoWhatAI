d0608f8d50a309f0026d74cc394fb91f296c7bd4
MattWate <matthew@dubyahinnovation.com>
2025-08-04 15:39:17 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,13 +2,11 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { resolve } from 'path'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  // Explicitly set the project root
-  root: resolve(__dirname, 'SoWhatAI'), 
+  // Explicitly set the project root to the current directory
+  root: __dirname,
   plugins: [react()],
   build: {
-    // The output directory for the build, relative to the root
     outDir: 'dist'
   }
 })
