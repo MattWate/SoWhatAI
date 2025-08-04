6cd9c8071ed73668da8a533d50f807b95ef4d59d
MattWate <matthew@dubyahinnovation.com>
2025-08-04 16:27:31 +0200

    Create vite.config.js

---
+++ SoWhatAI/vite.config.js
@@ -0,0 +1,7 @@
+// This file is now located at: SoWhatAI/vite.config.js
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+})
